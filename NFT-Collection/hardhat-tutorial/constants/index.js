132b513aa3b1fde144f73e6c3fff0ee8835cc9e3
Ilan Benichou <ilanben27@gmail.com>
2022-07-12 19:35:01 +0200

    feat(nft_collec): compile contract

---
+++ NFT-Collection/hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0xDF8674067334CF0d14092f0c50Cdf08167b3dF10";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-two-sand.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
